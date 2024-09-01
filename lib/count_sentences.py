7e923a5fb970625f3defa662281de661a1596695
Anne Liza Wangui <annewangui210@gmail.com>
2024-09-02 01:23:46 +0300

    python-p3-oo-counting-sentences-lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
-#!/usr/bin/env python3
+import re
+import sys
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = ''
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.", file=sys.stdout)
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        """Returns True if the value ends with a period."""
+        return self.value.endswith('.')
+
+    def is_question(self):
+        """Returns True if the value ends with a question mark."""
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        """Returns True if the value ends with an exclamation mark."""
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        """Counts the number of sentences in the value."""
+        # Use regex to split based on '.', '?', '!', but make sure to avoid empty splits
+        sentences = re.split(r'[.?!]+', self.value)
+        # Filter out any empty strings that result from split
+        return len([s for s in sentences if s.strip()])
